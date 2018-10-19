7108db72246edcbbd66230b39b29a57984aa9418
Austin Woo <austinwoo@Austins-MacBook-Pro.local>
2018-10-18 19:47:00 -0700

    Merged Order, Item, and Account Model

---
+++ project_shell/server/migrations/20181019024613-create-order.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      destination: {
+        type: Sequelize.STRING
+      },
+      creditCard: {
+        type: Sequelize.INTEGER
+      },
+      securityCode: {
+        type: Sequelize.INTEGER
+      },
+      expDate: {
+        type: Sequelize.INTEGER
+      },
+      priceTotal: {
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Orders');
+  }
+};
