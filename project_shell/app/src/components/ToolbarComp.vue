395af5fdf21fee1e683911c408310143f259a1e8
Ian Fong <ian.fong8@gmail.com>
2018-10-23 20:39:00 -0700

    Added broken Toolbar

---
+++ project_shell/app/src/components/ToolbarComp.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="navButton">
+		<div id="home">
+			<router-link to="/">Home</router-link>
+		</div>
+		<div id="services">
+			<router-link to="/services">Services</router-link>
+		</div>
+		<div id="manufacturers">
+			<router-link to="/manufacturers">Manufacturers</router-link>
+		</div>
+		<div id="tracking">
+			<router-link to="/tracking">Tracking</router-link>
+		</div>
+		<div id="login">
+			<router-link to="/login">Login</router-link>
+		</div>
+		<router-view/>
+	</div>
+
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class ToolbarComp extends Vue{}
+</script>
+<style lang ="scss">
+.navButton{
+	font-family: 'Roboto',san-serif;
+	background-color: #;
+}
+#home{
+	position:center;
+	padding:20px;
+	a{
+		font-weight: bold;
+		color: black;
+		&.router-link-exact-active{
+			color:white;
+		}
+	}
+}
+#services{
+	position:center;
+	padding:20px;
+	a{
+		font-weight: bold;
+		color: black;
+		&.router-link-exact-active{
+			color:white;
+		}
+	}
+}
+#manufacturers{
+	position:center;
+	padding:20px;
+	a{
+		font-weight: bold;
+		color: black;
+		&.router-link-exact-active{
+			color:white;
+		}
+	}
+}
+#tracking{
+	position:center;
+	padding:20px;
+	a{
+		font-weight: bold;
+		color: black;
+		&.router-link-exact-active{
+			color:white;
+		}
+	}
+}
+#login{
+	position:center;
+	padding:20px;
+	a{
+		font-weight: bold;
+		color: black;
+		&.router-link-exact-active{
+			color:white;
+		}
+	}
+}
+
+</style>
---
+++ project_shell/app/src/views/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+   	Toolbar Comp
+   	<ToolbarComp/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ToolbarComp from '@/components/ToolbarComp.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+  ToolbarComp,
+  },
+  })
+  //export default class Toolbar extends Vue {}
+</script>
