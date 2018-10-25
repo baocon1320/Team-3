1c7f8d775409b15d1e44db22d7b539fd51941094
Austin Woo <austinwoo@Austins-MacBook-Pro.local>
2018-10-25 14:44:03 -0700

    Added Tracking/Login page and some assets

---
+++ project_shell/app/src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <img id="image" src="../assets/bikeShop.jpg" alt="./assets/logo.png"></img>
+        <div id="logInDiv">
+            <form id="userInput">
+                <div id="userName">Username:</div>
+                <input id="userBox" type="text" name="userName"><br>
+                <div id="password">Password:</div>
+                <input id="passwordBox" type="text" name="password"><br>
+            </form>
+            <button id="trackingButton" type="button">Log In</button>
+        </div>  
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop} from 'vue-property-decorator';
+@Component
+export default class Login extends Vue{
+	@Prop() private msg!: string;
+}
+</script>
+<style lang ="scss">
+    #image{
+        float:left;
+        border-color: black;
+        left:10px;
+        padding-top:15px;
+        padding: 5px;
+        width: 800px;
+        height: 400px; 
+    }
+    #logInDiv{
+        float:right;
+        border-radius: 15px;
+        border-color: black;
+        background-color:#FFAB5D;
+        width: 300px;
+        height: 150px;
+        padding-right: 50px;
+        padding: 10px;
+        margin-top: 10%
+    }
+    #trackingButton{
+        margin:auto;
+        border-radius: 5px;
+        padding:10px;
+    }
+    
+</style>
---
+++ project_shell/app/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   	<Login  msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Login from '@/components/Login.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+  Login,
+  },
+  })
+  export default class LoginView extends Vue {}
+</script>
