a55a4e9c9988a7147e75d12029bb46a175949d5e
thomkaar <thomkaar@gmail.com>
2018-11-01 15:09:18 -0700

    Created a file for all routing to interact with Order

---
+++ project_shell/server/routes/order.js
@@ -0,0 +1,15 @@
+//This file is run when express encountes an http request starting with '/order'.
+var express = require('express');
+let OrderController = require('../controllers/order');
+
+const orderRoutes = express.Router();
+
+//Order Routes
+orderRoutes.get('/:id', OrderController.getOrderById);
+orderRoutes.get('/account', OrderController.getOrderByAccountId);
+orderRoutes.post('/create', OrderController.createOrder);
+orderRoutes.put('/addItem', OrderController.addItemToOrder);
+orderRoutes.put('/removeItem', OrderController.removeItemFromOrder);
+orderRoutes.delete('/deleteOrder', OrderController.deleteOrder);
+
+module.exports = orderRoutes;
