9afb35e7b330fd3ba04bc44709c074419a8fe833
Bao Nguyen <baocon1320@gmail.com>
2018-11-01 17:48:39 -0700

    add checkout cart item pages

---
+++ project_shell/app/src/components/ItemDetail.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class = "item_info">
+    <div class = "item_display">
+      <div class ="item_picture">
+        <a>
+          <img src = "../assets/bicycle1.jpg"></img>
+        </a>
+      </div>
+      <div class = "item_name">
+        <a >  <h2> Mountain Bicycle for Kid</h2> </a>
+        <h3> Trek Bicycle</h3>
+        <h4 class = "price"> <b> Price: </b> $200 </h4>
+        <button class= "add_cart">
+          <router-link to="/cart">Add to Cart</router-link>
+        </button>
+        <h5 class = "moreDetail">  3 in stock
+        </h5>
+      </div>
+    </div>
+    <div class = "item_description">
+      <div class = "item_title">
+        <h1> ITEM DESCRIPTION </h1>
+      </div>
+      <div class = "hori_line_"><span class = "hori_line"></span></div>
+      <div class = "item_specs">
+
+        <span>
+          When Trek built the Emonda S 4, they created an incredibly fast, fun machine. A bike that features a full carbon frame made from Trek's OCLV carbon, some of the best and lightest in the business. Light is one thing, but what really sets the Emonda apart is the ride. The specific carbon layup and the dialed geometry combine to give you that lively, yet stable feel that makes us get on our bikes in the first place. They didn't skimp on components either. A full 10-speed Shimano Tiagra gruppo and tubeless-ready Bontrager wheels grace the frame and blow the performance-to-value ratio out of the water.
+        </span>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ItemList extends Vue {
+  @Prop() private msg!: string;
+}
+
+</script>
+
+<style scoped lang="scss">
+a:link {
+    text-decoration: none;
+    color: white;
+}
+a:hover, a:active {
+    color: red;
+}
+.item_specs {
+  font-size: 14px;
+  text-align: justify;
+}
+.item_description {
+  letter-spacing: initial;
+  word-spacing: initial;
+}
+.item_title {
+
+  text-align: center;
+}
+.hori_line_ {
+  padding: 0 0 35px 0;
+}
+.hori_line {
+  background-color: #e5e5e5;
+  border-color: #e5e5e5;
+  color: #000;
+  height: 1px;
+  width: 100%;
+  display: block;
+}
+.add_cart {
+  margin-top: -15px;
+  font-size: 18px;
+}
+.item_info {
+  margin-top: 20px;
+  padding: 0 0 0 80px;
+  text-align: initial;
+  margin: 0 0 0 4px;
+  word-spacing: -4px;
+  letter-spacing: -4px;
+  line-height: 1.25;
+}
+.moreDetail {
+  color: red;
+}
+.item_name h5 {
+  line-height: 0.2;
+
+}
+.item_name h4 {
+  line-height: 1.5;
+  font-size: 16px;
+}
+.item_name h3 {
+  font-size: 14px;
+  margin-bottom: 5px;
+  word-wrap: break-word;
+}
+.item_name h2 {
+  font-size: 20px;
+  text-transform: uppercase;
+}
+.item_name {
+  display: inline-block;
+  text-align: initial;
+  position: relative;
+  top: 40px;
+  vertical-align: top;
+  overflow: hidden;
+  line-height: 0.1;
+
+}
+.item_picture {
+  width : 300px;
+  display: inline-block;
+  text-align: initial;
+}
+.item_picture a img {
+  vertical-align: top;
+  width: 200px;
+  height: 200px;
+}
+.item_display {
+  display: inline-block;
+  vertical-align: top;
+  overflow: hidden;
+  word-spacing: normal;
+  letter-spacing: normal;
+  padding: 0;
+  margin: 0;
+  max-width: 1250px;
+  position: relative;
+}
+</style>
---
+++ project_shell/app/src/views/CheckoutView.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class = "main_cart">
+    <div class = "cart_view">
+      <ShippingInfo/>
+      <div class = "oder_summary">
+        <div class = "cate_header"> Order Summary </div>
+        <div class = "hori_line_"><span class = "hori_line"></span></div>
+        <OrderSummary/>
+        <div class = "hori_line_"><span class = "hori_line"></span></div>
+        <button class = "button_checkout"> MAKE A PAYMENT </button>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import CartItem from '@/components/CartItem.vue'; // @ is an alias to /src
+import OrderSummary from '@/components/OrderSummary.vue';
+import ShippingInfo from '@/components/ShippingInfo.vue';
+
+@Component({
+  components: {
+    CartItem,
+    OrderSummary,
+    ShippingInfo
+  },
+})
+export default class CheckoutView extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+.button_checkout {
+  width: 380px;
+  height: 50px;
+  font-size: 25px;
+}
+.main_cart {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+  background: transparent;
+  max-height: 999999px;
+}
+.oder_summary {
+  bottom: auto;
+  float: right;
+  background: #fff;
+  padding: 4px;
+  width: 400px;
+  font-size: 13px;
+  font-weight: bold;
+  padding-right: 30px;
+}
+.cart_view {
+  position: relative;
+  max-height: 999999px;
+}
+
+.cate_header {
+  margin-bottom: 21px;
+  font-size: 24px;
+  line-height: 1;
+  min-height: 23px;
+  text-transform: uppercase;
+}
+
+.hori_line_ {
+  padding: 0 0 35px 0;
+}
+.hori_line {
+  background-color: #e5e5e5;
+  border-color: #e5e5e5;
+  color: #000;
+  height: 1px;
+  width: 100%;
+  display: block;
+}
+.result_list {
+  margin: 0 0 0 4px;
+  padding: 0;
+  /*line-height: 1.25; */
+}
+.list_item_display  {
+  padding: 0 0 0 25px;
+  text-align: initial;
+  font-size: 16px;
+}
+</style>
