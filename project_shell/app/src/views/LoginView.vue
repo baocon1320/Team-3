a12d6201904f62b3c3b3e73f60fa43550e864337
Bao Nguyen <baocon1320@gmail.com>
2018-11-13 18:27:16 -0800

    update the account provider

--- project_shell/app/src/views/LoginView.vue
+++ project_shell/app/src/views/LoginView.vue
@@ -1,29 +1,36 @@
+
 <template>
   <div class="home">
     <div id="loginBox">
         <img id='logo' src='../assets/foxycleLogo.png'></img>
    	    <v-form id='form' v-model="valid">
             <v-text-field
-            v-model="UserName"
+            v-model="username"
             label="UserName"
             required
             ></v-text-field>
             <v-text-field
-            v-model="Password"
+            v-model="password"
             label="Password"
             required
             ></v-text-field>
         </v-form>
-      <v-btn id="loginButton" outline absolute>
+      <v-btn id="loginButton" v-on:click="submit" outline absolute>
             Login
         </v-btn>
+        <h1> userName is : {{ account.username }} </h1>
+        <h1> passWord is : {{ account.password}} </h1>
+        
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import Login from '@/components/Login.vue'; // @ is an alias to /src
+import { AccountModel } from '@/providers/account';
+import { AccountProvider } from '@/providers/account';
+import axios from 'axios';
 
 @Component({
   components: {
@@ -31,6 +38,24 @@ import Login from '@/components/Login.vue'; // @ is an alias to /src
   },
   })
   export default class LoginView extends Vue {
+    account: AccountModel = new AccountModel('','',0);
+    new_account!: AccountModel;
+    accountprovider: AccountProvider = new AccountProvider();
+    username: string = '';
+    password: string = '';
+    
+    mounted() {
+      this.accountprovider.getAccountById(2).then(data => {
+        this.account = data;
+      })     
+    }
+    submit() {
+      this.new_account =  new AccountModel(this.username, this.password, 5);
+      this.accountprovider.createAccount(this.new_account).then (data => {
+        this.account = data;
+      })
+    }
+
   }
 </script>
 
