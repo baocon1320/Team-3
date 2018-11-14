e24672ae8fcf762a1c7a3fc7c098dca66fa393bb
Bao Nguyen <baocon1320@gmail.com>
2018-11-13 20:09:48 -0800

    fixed general controller

--- project_shell/server/controllers/general.js
+++ project_shell/server/controllers/general.js
@@ -13,17 +13,24 @@ exports.getGeneralById = async(req,res) => {
 
 //TODO add validation for general
 exports.updateGeneral = async(req,res) => {
-	General.update(req.body, { where: { id: req.params.id} }).then((affected) => {
-		if(affected != null){
-			if(affected[0] > 0){
-					res.send(202);
+	try{
+
+		General.update(req.body, { where: { id: req.params.id} }).then((affected) => {
+			if(affected != null){
+				if(affected[0] > 0){
+						res.send(202);
+				}else{
+					//TODO: Fix this to reflect the actual error
+					res.send(404);
+				}
 			}else{
-				//TODO: Fix this to reflect the actual error
 				res.send(404);
 			}
-		}else{
-			res.send(404);
-		}
-	});
+		});
+	} 
+	catch(err){
+		console.log(err)
+	}
+	
 };
 
